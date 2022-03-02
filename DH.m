89d809b2958159f4b21374483943a51566aa88e2
Soham Dasgupta <59978919+astonishingwolf@users.noreply.github.com>
2022-03-02 14:16:35 +0530

    sv

--- DH.m
+++ DH.m
@@ -1,10 +1,10 @@
 function [Trans] = DH(theta,d,alpha,a)
 theta = deg2rad(theta);
 alpha = deg2rad(alpha);
-ct = cos(theta);
-st = sin(theta);
-ca = cos(alpha);
-sa = sin(alpha);
+ct = round(cos(theta),3);
+st = round(sin(theta),3);
+ca = round(cos(alpha),3);
+sa = round(sin(alpha),3);
 Trans = [ct , -ca*st , st*sa , a*ct;
          st , ca*ct ,-sa*ct , a*st;
          0  , sa    , ca    , d   ;
