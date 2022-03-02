ec333054f723de87c55c1c83397393517aeb4206
Mukesh thota <mukeshthotampc@gmail.com>
2022-03-02 11:59:05 +0530

    jniukyj

--- drawpad.m
+++ drawpad.m
@@ -3,7 +3,10 @@ y = sym('y','real');
 z = sym('z','real');
 H1 = homo(0,1,0,-45,0,0,0);
 H2 = homo(0,0,1,90,0,0,0);
-H3 = homo(0,0,1,0,1,2,1);
+H3 = homo(0,0,0 ,0,1,2,1);
 R = [1;2;0;1];
-a = @(x,y,z)[x;y;z;1];
-ans = (H1*H2*H3*a(x,y,z));%Frame C
+%a = @(x,y,z)[x;y;z;1];
+%ans = (H1*H2*H3*a(x,y,z));%Frame C
+i=H1*H2
+inv(i)*R
+
